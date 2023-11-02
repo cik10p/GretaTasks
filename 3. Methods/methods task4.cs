9d9d05babee8ffc70541fd28ca1ba619da83d601
DMIJA_LT <jazel.d@outlook.com>
2023-11-02 18:31:39 +0200

    2 new method tasks

---
+++ 3. Methods/methods task4.cs
@@ -0,0 +1,11 @@
+// 1) Sukurti metoda kuris patikrina ar skaicius dalinasi is 2ju.
+// 2) 7 eiluteje yra salyga kuri patikrina ar skaicius 7 dalinasi is 2ju.
+// 3) Sukurti metoda kuris grazintu fraze "skaicius dalynamas is dveju" jeigu skaicius gali buti padalinamas. Arba priesingu atveju turi grazinti "skaicius nedalynamas is dveju".
+
+
+// patikrinimo salyga.
+if (7 % 2 == 0) {
+   // Skaicius dalinamas is dveju
+} else {
+   // Skaicius nedalinamas is dveju
+}
---
+++ 3. Methods/methods task5.cs
@@ -0,0 +1,23 @@
+/*
+Dabaigti koda, kad konsole atvaizduotu toki rezultata:
+
+Sveiki!
+Siandienos skaicius buvo laimingas
+Viso gero
+*/
+
+string tikrintiSkaiciu(int skaicius){
+    if(skaicius == 7)
+	{
+        return "laimingas";
+    }
+	else
+	{
+		return "nelaimingas";
+	}
+}
+
+void start()
+{
+    Console.WriteLine("Kaunas");   
+}
