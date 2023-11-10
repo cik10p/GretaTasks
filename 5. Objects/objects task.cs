13c3a1327b42bfd79cc70a926678548ec89e20ef
DMIJA_LT <jazel.d@outlook.com>
2023-11-10 23:54:02 +0200

    Adding Object tasks

---
+++ 5. Objects/objects task.cs
@@ -0,0 +1,26 @@
+/*
+1) Sukurti naują projektą pavadinimu "Registration sistem" ir sukurti folderį "Objektai" kuriame bus saugomi objektų klasės.
+2) Sukurti Klasę "Person" su parametrais ir konstruktorium (pasinaudot pavizdžiu kurį padarėm paskaitos metu): 
+firstName - tekstas,
+lastName- tekstas,
+age - sveikasis skaicius,
+phoneNumber - tekstas,
+sallary - skaicius su kableliu (duomenų tipas: double),
+address - tekstas,
+isAlergic - Boolean'as (duomenų tipas: bool),
+
+3) Tame pačiame folderije sukurti dar vieną klasę pavadinimu "Product" su parametrais:
+title - tekstas,
+price - skaicius su kableliu (duomenų tipas: double),
+
+4) Taip pat sukurti klasę pavadinimu "Animal" su parametrais:
+spieces - tekstas,
+name - tekstas,
+age - sveikasis skaicius,
+
+5) Program.cs sukurti
+3 Person tipo objektus.
+5 Product tipo objektus
+ir 2 Animal tipo objektus
+
+*/
